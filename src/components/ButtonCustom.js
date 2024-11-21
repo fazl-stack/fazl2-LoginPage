4a062c9769713acc24abe3447c5c9d6cacdf83b8
fazl-stack <nexusnervous176@gmail.com>
2024-11-21 09:10:06 +0700

    first commit

---
+++ src/components/ButtonCustom.js
@@ -0,0 +1,42 @@
+import React, {useState} from 'react';
+import {
+  View,
+  Text,
+  Switch,
+  ScrollView,
+  Touchable,
+  TouchableOpacity,
+} from 'react-native';
+
+const ButtonCustom = ({
+  textColor = 'white',
+  tallHeight = 50,
+  background = 'black',
+  kepala,
+  verif,
+}) => {
+  return (
+    <TouchableOpacity
+      style={{
+        alignSelf: 'center',
+        backgroundColor: kepala ? 'white' : 'black',
+        height: tallHeight,
+        width: 250,
+        borderRadius: 50,
+        margin: 50,
+        elevation: 10,
+      }}
+      onPress={verif}>
+      <Text
+        style={{
+          color: kepala ? 'black' : 'white',
+          textAlign: 'center',
+          padding: 15,
+        }}>
+        Sign In
+      </Text>
+    </TouchableOpacity>
+  );
+};
+
+export default ButtonCustom;
